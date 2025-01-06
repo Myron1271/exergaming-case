755741472f5ed5c229dab61d6ee6972c06490177
Myron <myronmax_seelen@live.nl>
2025-01-06 01:05:41 +0100

    Old Pages Removed and New Paged Added

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,24 @@ import MapView from '@/views/MapView.vue'
 import QRCodeScan from '../views/QRCodeScan.vue'
 import AboutView from '../views/AboutView.vue'
 import TotalPointsView from '../views/TotalPointsView.vue'
+import HomeView from '../views/HomeView.vue'
+import FirstMurder from '../views/FirstMurder.vue'
+import SecondMurder from '../views/SecondMurder.vue'
+import ThirdMurder from '../views/ThirdMurder.vue'
+import MurdererFound from '../views/MurdererFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/map',
+      redirect: '/home',
 
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
     },
     {
       path: '/map',
@@ -18,22 +28,24 @@ const router = createRouter({
       component: MapView,
     },
     {
-      path: '/qrcodescan',
-      name: 'qrcodescan',
-      component: QRCodeScan,
+      path: '/firstmurder',
+      name: 'firstmurder',
+      component: FirstMurder,
     },
-    /* {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: AboutView,
-    }, */
     {
-      path: '/totalpoints',
-      name: 'totalpoints',
-      component: TotalPointsView,
+      path: '/secondmurder',
+      name: 'secondmurder',
+      component: SecondMurder,
+    },
+    {
+      path: '/thirdmurder',
+      name: 'thirdmurder',
+      component: ThirdMurder,
+    },
+    {
+      path: '/murdererfound',
+      name: 'murdererfound',
+      component: MurdererFound,
     },
   ],
 })
