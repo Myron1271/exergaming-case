f893341f5060cf26193b78acc2d05ad1b6238523
Myron <myronmax_seelen@live.nl>
2025-01-12 20:46:53 +0100

    Example Code added to Account Page

---
+++ src/views/AccountView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Welkom {{ userName }}!</h1>
+    </div>
+</template>
+<script>
+import Modal from '@/components/Modal.vue';
+export default {
+    name: 'AccountView',
+    components: {Modal},
+    data() {
+        return {
+            userName: localStorage.getItem("userName"),
+        }
+    }
+    
+}
+</script>
+<style>
+    
+</style>
